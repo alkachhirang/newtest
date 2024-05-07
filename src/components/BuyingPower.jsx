56d5babc1744ad7fb678b54294df13d1b52e0d11
alkachhirang <alkachhirang3@gmail.com>
2024-05-07 09:33:20 +0530

    changes

--- src/components/BuyingPower.jsx
+++ src/components/BuyingPower.jsx
@@ -14,7 +14,7 @@ const BuyingPower = () => {
                 </div>
                 <div className='flex flex-wrap flex-row justify-between mb-[30px] md:mb-[52px] lg:mb-[82px]'>
                     <div className='lg:w-[49%] w-full relative'>
-                        <div className='bg-gradient-to-br from-[#33A8FF] to-[#034BDA] rounded-[24px] md:pt-[47.75px] md:pl-[82.32px] md:pr-[37.48px] p-[25px] lg:max-w-[558px] w-full pb-[30px] lg:pb-0 md:max-h-[274px] md:h-full'>
+                        <div className='bg-gradient-to-br from-[#33A8FF] to-[#034BDA] rounded-[24px] md:pt-[47.75px] md:pl-[82.32px] md:pr-[37.48px] p-[16px] sm:p-[25px] lg:max-w-[558px] w-full pb-[30px] lg:pb-0 md:max-h-[274px] md:h-full'>
                             <div className='flex max-sm:flex-col gap-[16.4px] items-center'>
                                 <img src={johnimg} alt="johnimg" className='max-w-[72px]' />
                                 <p className='leading-[150%] text-[#fff] text-xs '><span className='font-bold'>John</span> invested $250 without leverage, so his buying power was $250, and he could potentially earn up to $40.</p>
@@ -25,7 +25,7 @@ const BuyingPower = () => {
                         </div>
                     </div>
                     <div className='lg:w-[49%] w-full mt-[30px] !sm:mt-[50px]  lg:mt-0'>
-                        <div className='bg-gradient-to-br from-[#33A8FF] to-[#034BDA] rounded-[24px] md:pt-[20px] md:pl-[22px] md:pr-[31.2px] p-[25px] lg:max-w-[558px] w-full pb-[20px] lg:pb-0 md:max-h-[274px] md:h-full'>
+                        <div className='bg-gradient-to-br from-[#33A8FF] to-[#034BDA] rounded-[24px] md:pt-[20px] md:pl-[22px] md:pr-[31.2px] p-[16px] sm:p-[25px] lg:max-w-[558px] w-full pb-[20px] lg:pb-0 md:max-h-[274px] md:h-full'>
                             <div className='flex max-sm:flex-col gap-[16.4px] items-center'>
                                 <img src={mikeimg} alt="mikeimg" className='max-w-[72px]' />
                                 <p className='leading-[150%] text-[#fff] text-xs '><span className='font-bold'>Mike</span> invested the same amount but utilized leverage of 1 to 30, giving him a buying power of $7,500.
@@ -38,7 +38,7 @@ const BuyingPower = () => {
                     </div>
                 </div>
                 <div className='flex justify-center items-center flex-col' data-aos="fade-up">
-                    <button className="bg-gradient-to-br from-[#034BDA] btn to-[#33A8FF] shadow-[0_2px_10px_0_rgba(0,_0,_0,_0.15)] leading-[150%] capitalize text-nowrap transition-all ease-linear duration-500 py-[12px] px-[31.4px] text-xs font-semibold text-[#FFFFFF] rounded-[58px]">
+                    <button className="bg-gradient-to-br from-[#034BDA] border-none btn to-[#33A8FF] shadow-[0_2px_10px_0_rgba(0,_0,_0,_0.15)] leading-[150%] capitalize text-nowrap transition-all ease-linear duration-500 py-[12px] px-[31.4px] text-xs font-semibold text-[#FFFFFF] rounded-[58px]">
                         Maximize Your Profit
                     </button>
                     <p className='leading-[160%] text-[#020507] text-xs capitalize mt-[13px] font-normal text-center '>With Leveraged Accounts and Enhanced Buying Power Today!</p>
