0331c3571fece64b83fefec7325138dfd7bf1fb8
alkachhirang <alkachhirang3@gmail.com>
2024-05-07 09:22:47 +0530

    change

--- src/components/InvestStock.jsx
+++ src/components/InvestStock.jsx
@@ -30,7 +30,7 @@ const InvestStock = () => {
                 </div>
                 <div className='flex max-lg:flex-col justify-center items-center gap-[18px] lg:gap-[22px]' data-aos="fade-up">
                     <p className='font-normal text-sm text-[#101010] leading-[130%]'>and a 100s of reasons</p>
-                    <button className="bg-gradient-to-br from-[#034BDA] to-[#33A8FF] shadow-[0_2px_10px_0_rgba(0,_0,_0,_0.15)] btn leading-[150%] capitalize text-nowrap transition-all ease-linear duration-500 py-[11px] px-[29.1px] text-xs font-semibold text-[#FFFFFF] rounded-[58px]  glow-on-hover before:absolute before:top-[-2px] before:left-[-2px] before:z-[-1] before:opacity-0 before:rounded-[50px] after:z-[-1] after:absolute after:w-full after:h-full after:left-0 after:top-0 after:rounded-[50px] hover:before:opacity-[1] relative z-0 outline-none">
+                    <button className="bg-gradient-to-br from-[#034BDA] to-[#33A8FF] shadow-[0_2px_10px_0_rgba(0,_0,_0,_0.15)] btn leading-[150%] capitalize text-nowrap transition-all ease-linear duration-500 py-[11px] px-[29.1px] text-xs font-semibold text-[#FFFFFF] rounded-[58px]">
                         Invest Now
                     </button>
                 </div>
